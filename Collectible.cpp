a5faf5c3d0458294d1c3cc0ae1ed5d349cdbef9d
alchevalie6 <alchevalie6@ada.local.isima.fr>
2022-05-14 22:47:00 +0200

    ajout méthode afficher (pas tester parce que flemme)

---
+++ Collectible.cpp
@@ -0,0 +1,10 @@
+#include "Collectible.hpp"
+
+void Collectible::afficher(SDL_Renderer* renderer,int posx, int posy)
+{
+    SDL_Rect destination;
+    SDL_QueryTexture(_texture,NULL,NULL,&destination.w,&destination.h);
+    destination.x=posx-_x+WSCREEN/2-WPERSO/2;
+    destination.y=posy-_y+HSCREEN/2-HPERSO/2;
+    SDL_RenderCopy(renderer,_texture,NULL,&destination);
+}

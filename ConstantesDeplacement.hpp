4ecc8908a918c326a0f5eabd4da14e6e10f05bb6
Nathan DESNOS <desnos.nathan@gmail.com>
2022-05-14 23:02:24 +0200

    Personnage avec fonctions de déplacement probablement puories du cul

--- ConstantesDeplacement.hpp
+++ ConstantesDeplacement.hpp
@@ -9,9 +9,17 @@ enum Direction {
     Haut
 };
 
-const int gravite = 5;
 const int dimCase = 50;
 const int hauteurPerso = 1.6 * dimCase;
 const int largeurPerso = dimCase;
+const int mapH = 600;
+const int mapW = 1000;
+const int mapMaxColumn = mapW / dimCase;
+const int mapMaxRow = mapH / dimCase;
+
+const int gravite = 5;
+const int vitesseSaut = 30;
+const int vitesseLaterale = 5;
+const int vitesseGrimper = 5;
 
 #endif

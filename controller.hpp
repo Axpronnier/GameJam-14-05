cdeb080b8616e5d3092fbcc4e65119a6b5453b0a
axpronnier <axel.pronnier@gmail.com>
2022-05-15 12:20:08 +0200

    continuation controller createur

--- controller.hpp
+++ controller.hpp
@@ -14,11 +14,11 @@ class Controller {
 public:
     Controller ();
     void controller(bool *, int, int);
+    int ** _map;
 private:
     Perso _personnage;
     int _mapLines;
     int _mapCol;
-    int ** _map;
     Collectible * _collectibles;
     int _ncollectibles;
     Interactible * _interactibles;

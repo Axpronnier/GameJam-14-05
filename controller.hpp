2feb978c8157d7912e0199b2f2e49e8ccb8a7f95
axpronnier <axpronnier@ada.local.isima.fr>
2022-05-15 13:17:36 +0200

    dsl nathan

--- controller.hpp
+++ controller.hpp
@@ -14,11 +14,10 @@ class Controller {
 public:
     Controller ();
     void controller(bool *, int, int);
-private:
+    int ** _map;
     Perso _personnage;
     int _mapLines;
     int _mapCol;
-    int ** _map;
     Collectible * _collectibles;
     int _ncollectibles;
     Interactible * _interactibles;
